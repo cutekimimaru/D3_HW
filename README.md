510027c1569ac34089b2400e6e9c48501143609e
ChenDai-sze <cutekimimaru@hotmail.com>
2016-10-24 00:55:35 +0800

    finished D3_HW-03

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-#### D3 Assignment Part 2
+#### D3 Assignment Part 3
 
 ###### 繳交內容如下
-1. 瑪麗歐水管
-2. N~M之間的亂數
-3. 一個亂數函式: random(n, m) 可以在chrome console測試
+1. 畫出五線譜
+2. 亂數長條圖
--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function getRandomInt(n, m) {
     console.log("your input", n, m);
     return Math.ceil((m-n)*Math.random()+n);
 }
+
+// draw the five lines
+function drawLines () {
+    for (var i=0; i<5; i=i+1) {
+        d3.select(".melody>svg")
+          .append("line")
+          .attr({
+            x1: 0,
+            y1: 45 + i * 30,
+            x2: 300,
+            y2: 45 + i * 30,
+            stroke: "black",
+            "stroke-width": 2
+            });    
+    }
+}
